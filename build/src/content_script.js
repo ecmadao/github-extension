ef830bedcc60bf5b028e1798a11846719a5a3ae8
ecmadao <wlec@outlook.com>
2016-10-17 12:16:56 +0800

    stop default event

--- build/src/content_script.js
+++ build/src/content_script.js
@@ -3,8 +3,17 @@ function initialExtension() {
   $recentBranches.find('.RecentBranches-item').find('a').on('click', function(e) {
     e.preventDefault();
     e.stopPropagation();
+    var href = $(this).attr('href');
+    var hrefSplits = href.split('/');
+    var lastSplit = hrefSplits.slice(-1);
+    var branchName = lastSplit[0].split('?')[0];
+    var newHref = hrefSplits.slice(0, -1);
+    newHref.push(branchName + '...' + lastSplit[0]);
+    window.location.href = newHref.join('/');
+    return false;
   });
 }
 
-initialExtension();
-console.log('====================initialExtension==========================')
+setTimeout(function() {
+  initialExtension();
+}, 1500);
