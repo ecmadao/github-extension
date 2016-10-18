bcccacdc19e0523431893e891fc04a3fe233b5c9
ecmadao <wlec@outlook.com>
2016-10-19 01:10:46 +0800

    refactor

--- build/src/code_review_enhance.js
+++ build/src/code_review_enhance.js
@@ -1,76 +1,3 @@
-function listenWindowScroll() {
-  var $document = $(document);
-  var $actionButton = $('.git_action_button_wrapper');
-  $(window).scroll(function() {
-    var $currentTop = $document.scrollTop();
-    if ($currentTop > 300) {
-      $actionButton.addClass('active');
-    } else {
-      $actionButton.removeClass('active');
-    }
-  });
-}
-
-function addScrollButton() {
-  var scrollButton = $(actionButtonTemplate());
-  $('body').append(scrollButton);
-  bindButtonAction();
-}
-
-function bindButtonAction() {
-  var $scrollButton = $('.github_scroll_top');
-  $scrollButton.on('click', function() {
-    $('html, body').animate({
-      scrollTop: 0
-    }, 'slow');
-  });
-}
-
-function actionButtonTemplate() {
-  return "<div class='git_action_button_wrapper'>" +
-  scrollButtonTemplate() +
-  topBarButtonTemplate() +
-  "</div>";
-}
-
-function scrollButtonTemplate() {
-  return "<div class='git_action_button github_scroll_top'>" +
-  "<div class='scroll_top_icon'></div>" +
-  "</div>";
-}
-
-function topBarButtonTemplate() {
-  return "<div class='code_review_tab_wrapper'>" + getTopbarButton() + "</div>";
-}
-
-function getTopbarButton(){
-  var $topbar = $('.tabnav-pr').find('.tabnav-tabs');
-  if (!$topbar || !$topbar.length) {
-    return "";
-  }
-  var tabs = [];
-  $topbar.find('a.tabnav-tab').each(function(index, tab) {
-    var href = $(tab).attr('href');
-    var svg = $(tab).find('svg')[0].outerHTML;
-    tabs.push({
-      href: href,
-      svg: svg
-    });
-  });
-  var tabTemplates = [];
-  tabs.forEach(function(tab, index) {
-    tabTemplates.push(topBarTemplate(tab));
-  });
-  return tabTemplates.join("");
-}
-
-function topBarTemplate(tabObj) {
-  var actionButtonClass = activePage(tabObj.href) ? 'git_action_button code_review_tab active' : 'git_action_button code_review_tab';
-  return "<a href='" + tabObj.href + "' class='" + actionButtonClass + "'>" +
-  "<div class='code_review_icon'>" + tabObj.svg + "</div>" +
-  "</a>";
-}
-
 function matchUrl(url) {
   return url.match(/\/pull\//) || url.match(/\/compare\//);
 }
@@ -82,12 +9,89 @@ function activePage(url) {
   return result && result.length === 1;
 }
 
-function initial() {
-  var url = window.location.href;
-  if (matchUrl(url)) {
-    addScrollButton();
-    listenWindowScroll();
-  }
-}
+var ActionButton = function() {};
+ActionButton.prototype = {
+  initial: function() {
+    var url = window.location.href;
+    if (matchUrl(url)) {
+      this._addScrollButton();
+      this._listenWindowScroll();
+    }
+  },
 
-initial();
+  _addScrollButton: function() {
+    var scrollButton = $(this._actionButtonTemplate());
+    $('body').append(scrollButton);
+    this._bindButtonAction();
+  },
+
+  _listenWindowScroll: function() {
+    var $document = $(document);
+    var $actionButton = $('.git_action_button_wrapper');
+    $(window).scroll(function() {
+      var $currentTop = $document.scrollTop();
+      if ($currentTop > 300) {
+        $actionButton.addClass('active');
+      } else {
+        $actionButton.removeClass('active');
+      }
+    });
+  },
+
+  _bindButtonAction: function() {
+    var $scrollButton = $('.github_scroll_top');
+    $scrollButton.on('click', function() {
+      $('html, body').animate({
+        scrollTop: 0
+      }, 'slow');
+    });
+  },
+
+  _actionButtonTemplate: function() {
+    return "<div class='git_action_button_wrapper'>" +
+    this.scrollButtonTemplate +
+    this.topBarButtonTemplate +
+    "</div>";
+  },
+
+  get scrollButtonTemplate() {
+    return "<div class='git_action_button github_scroll_top'>" +
+    "<div class='scroll_top_icon'></div>" +
+    "</div>";
+  },
+
+  get topBarButtonTemplate() {
+    return "<div class='code_review_tab_wrapper'>" + this.topbarButton + "</div>";
+  },
+
+  get topbarButton() {
+    var $topbar = $('.tabnav-pr').find('.tabnav-tabs');
+    if (!$topbar || !$topbar.length) {
+      return "";
+    }
+    var tabs = [];
+    $topbar.find('a.tabnav-tab').each(function(index, tab) {
+      var href = $(tab).attr('href');
+      var svg = $(tab).find('svg')[0].outerHTML;
+      tabs.push({
+        href: href,
+        svg: svg
+      });
+    });
+    var tabTemplates = [];
+    tabs.forEach((tab, index) => {
+      tabTemplates.push(this._getTopBarTemplate(tab));
+    });
+    return tabTemplates.join("");
+  },
+
+  _getTopBarTemplate: function(tabObj) {
+    var actionButtonClass = activePage(tabObj.href) ? 'git_action_button code_review_tab active' : 'git_action_button code_review_tab';
+    return "<a href='" + tabObj.href + "' class='" + actionButtonClass + "'>" +
+    "<div class='code_review_icon'>" + tabObj.svg + "</div>" +
+    "</a>";
+  }
+};
+
+const actionButton = new ActionButton();
+actionButton.initial();
